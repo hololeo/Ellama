0ee044a4dde9a0a749cf75befcbd861821c88ff1
UX Hololeo <11970940+hololeo@users.noreply.github.com>
2024-09-19 23:41:27 -0700

    initial upload

---
+++ ell-joker.py
@@ -0,0 +1,40 @@
+import ell
+from openai import OpenAI
+import os
+
+MODEL = "llama3.1:8b"
+
+#MODEL = "qwen2.5:0.5b"
+MODEL = "qwen2.5:1.5b"
+client = OpenAI(
+	base_url = "http://localhost:11434/v1",
+	api_key = "ollama",
+)
+
+ell.config.verbose = True
+ell.config.register_model(MODEL, client)
+
+@ell.tool()
+def say_it(text: str):
+    print (f"here is your joke:{text}")
+    os.system(f"say {text}")
+    return "Done"
+
+@ell.complex(model=MODEL, client=client, tools=[say_it])
+def make_joke(topic:str):
+    """You are a funny but corny comedy writer. Return a joke about the users topic. Review your joke. Remove emojis. Only return the joke. Do not provide commentary about the joke"""
+    return f"come up with a joke about {topic}. In your response do not include emojis. Do not include any commentary on the joke."    
+
+if __name__ == "__main__":
+    user_prompt = "AI"
+    callback = make_joke (user_prompt)
+    print (callback)
+    callback.tool_calls[0]()
+
+
+
+
+
+
+
+
